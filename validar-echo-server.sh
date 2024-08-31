2df185c576d621256da4437eff4a206de417a3c3
pgallino <pedrogallino11@gmail.com>
2024-08-31 16:53:31 -0300

    ejercicio 3

---
+++ validar-echo-server.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Configuración
+SERVER_CONTAINER_NAME="server"
+CONFIG_FILE_PATH="./server/config.ini"
+MESSAGE="validar echo server"
+
+# Como el puerto puede variar lo leo directamente del config
+SERVER_PORT=$(grep "SERVER_PORT" $CONFIG_FILE_PATH | awk -F '=' '{print $2}' | xargs)
+
+# Ejecuta netcat desde un contenedor temporal en la misma red
+RESPONSE=$(docker run --rm --network tp0_testing_net busybox sh -c "echo -n '$MESSAGE' | nc $SERVER_CONTAINER_NAME $SERVER_PORT")
+
+# Verifica si el mensaje recibido es igual al mensaje enviado
+if [ "$RESPONSE" == "$MESSAGE" ]; then
+    echo "action: test_echo_server | result: success"
+else
+    echo "action: test_echo_server | result: fail"
+fi
