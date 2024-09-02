862107478d4735c02bf212bfdc9af4d107e101c8
pgallino <pedrogallino11@gmail.com>
2024-09-02 16:24:58 -0300

    cambio la version de busybox

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 # Configuración
+SERVER_CONTAINER_NAME="server"
+CONFIG_FILE_PATH="./server/config.ini"
 MESSAGE="validar echo server"
 
+# Como el puerto puede variar lo leo directamente del config
+SERVER_PORT=$(grep "SERVER_PORT" $CONFIG_FILE_PATH | awk -F '=' '{print $2}' | xargs)
+
 # Ejecuta netcat desde un contenedor temporal en la misma red
-RESPONSE=$(docker run --rm --network tp0_testing_net busybox sh -c "echo -n '$MESSAGE' | nc server 12345")
+RESPONSE=$(docker run --rm --network tp0_testing_net busybox:latest sh -c "echo -n '$MESSAGE' | nc $SERVER_CONTAINER_NAME $SERVER_PORT")
 
 # Verifica si el mensaje recibido es igual al mensaje enviado
 if [ "$RESPONSE" = "$MESSAGE" ]; then
